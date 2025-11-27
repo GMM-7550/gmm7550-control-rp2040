05c74d091ee05137d7980c842119bcd1c880e52a
Anton Kuzmin <ak@gmm7550.dev>
2025-11-27 09:48:06 +0100

    Minor headers cleanup

--- src/include/gmm7550_control.h
+++ src/include/gmm7550_control.h
@@ -1,5 +1,20 @@
+/*
+ * Raspberry Pi RP2040 Control firmware for GMM-7550 module
+ * https://www.gmm7550.dev/doc/rp2040.html
+ *
+ * Copyright (c) 2025 Anton Kuzmin <ak@gmm7550.dev>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define GREEN_LED_PIN 25 /* GPIO 25 on Pico-based prototype, 0 on a final h/w */
+
+#define GMM7550_UART 0
+#define GMM7550_UART_TX_PIN 12
+#define GMM7550_UART_RX_PIN 13
+
 #include "queue.h"
 extern QueueHandle_t serial_rxQueue, serial_txQueue;
-
 extern void serial_init(__unused void *params);
+
 extern void usb_task(__unused void *params);
--- src/include/pico_gmm7550.h
+++ src/include/pico_gmm7550.h
@@ -7,16 +7,4 @@
  * SPDX-License-Identifier: MIT
  */
 
-#if 0
-#  define PICO_DEFAULT_UART 1
-#  define PICO_DEFAULT_UART_TX_PIN 8
-#  define PICO_DEFAULT_UART_RX_PIN 9
-#endif
-
-#define GREEN_LED_PIN 25 /* GPIO 25 on Pico-based prototype, 0 on a final h/w */
-
-#define GMM7550_UART 0
-#define GMM7550_UART_TX_PIN 12
-#define GMM7550_UART_RX_PIN 13
-
 #include <boards/pico.h>
